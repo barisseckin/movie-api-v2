231cec1ec2229d49eee8ab1000cca7c4933ce256
Baris0 <barisseckin0@gmail.com>
2022-08-15 22:49:45 +0300

    bugfix

--- src/main/java/com/movieappV2/dto/request/CreateCategoryRequest.java
+++ src/main/java/com/movieappV2/dto/request/CreateCategoryRequest.java
@@ -1,12 +1,10 @@
 package com.movieappV2.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@AllArgsConstructor
 public class CreateCategoryRequest {
 
     @NotBlank
